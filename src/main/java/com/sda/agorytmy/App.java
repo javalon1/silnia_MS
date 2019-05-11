b1ab7b867fc59eea224a667c7c4836a1683d88a2
Mateusz Suchan/suchy <mateusz.suchan93@gmail.com>
2019-05-11 15:41:41 +0100

    ciagi_i_silnia

--- src/main/java/com/sda/agorytmy/App.java
+++ src/main/java/com/sda/agorytmy/App.java
@@ -2,12 +2,51 @@ package com.sda.agorytmy;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static double ciag(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 0.5;
+        }
+        return ciag(n - 1) * ciag(n - 2);
     }
+
+    public static int ciag2 (int a){
+        if (a==1){
+            return -1;
+        }
+        else {
+            return -ciag2(a-1)*a-3;
+        }
+    
+
+}
+
+
+    public static int silnia(int n) {
+        System.out.println(n);
+        if (n < 2) {
+            return 1;
+        }
+        return n * silnia(n - 1);
+
+
+    }
+
+    public static void main(String[] args) {
+
+        silnia(5);
+
+        int silnia = 5;
+        int wynik = 1;
+        for (int i = 1; i <= silnia; i++) {
+            wynik = wynik * i;
+        }
+        System.out.println("Silnia:" + wynik);
+    }
+
 }
